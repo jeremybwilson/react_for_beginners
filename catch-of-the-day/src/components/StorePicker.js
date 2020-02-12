ff04cd1b297410846279eb63bf99c14ec0e9be67
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-02-11 16:02:21 -0800

    Catch of the Day
    - committing files from this practice folder

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+  
+  static propTypes = {
+    history: PropTypes.object
+  }
+  
+  goToStore = (event) => {
+    // 1. stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from that input
+    const storeName = this.myInput.current.value;
+    // console.log(this.myInput.current.value);
+    // 3. Change the page to to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  componentDidMount() {
+    console.log('MOUNTED!');
+    console.log(this);
+  };
+  
+  render(){
+    return (
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
+        { /* comment */ }
+        <h2>Please Enter A Store</h2>
+        <input 
+          type="text" 
+          ref={this.myInput} 
+          required 
+          placeholder="Store Name" 
+          defaultValue={getFunName()} 
+        />
+        <button type="submit">Visit Store -></button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
